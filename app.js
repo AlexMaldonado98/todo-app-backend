c4b22887b794a3a48e7a475c00f1b79107fea071
Alex Maldonado Noh <alex.maldonado2919@gmail.com>
2022-11-01 16:25:21 -0600

    build frontend added

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors());
 app.use(express.json());
 app.use(middleware.tokenExtractor);
+app.use(express.static('./build'));
 
 app.use('/api/user',userRouter);
 app.use('/api/task',taskRouter);
